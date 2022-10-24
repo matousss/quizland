293eefddc2edb9c5c59dbd4b6ef446ff73c4e1d3
matousss <matousss33@gmail.com>
2022-10-24 10:10:48 +0200

    Lil Change

--- db/models/index.js
+++ db/models/index.js
@@ -2,14 +2,14 @@
 
 const fs = require('fs');
 const path = require('path');
-const {Sequelize, DataTypes} = require('sequelize-typescript');
+const {Sequelize, DataTypes} = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config.js')[env];
 const db = {};
 
-let sequelize: Sequelize;
+let sequelize;
 if (config.use_env_variable) {
     sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,14 +2,14 @@ import NextAuth from "next-auth"
 import SequelizeAdapter from "@next-auth/sequelize-adapter"
 import CredentialsProvider from "next-auth/providers/credentials"
 
-const sequelize = require('/db/models').sequelize
+const sequelize = require('/db')
 
-export default NextAuth({
+const options = {
     providers: [
         CredentialsProvider({
             name: 'Credentials',
             credentials: {
-                username: {label: "Username", type: "text", placeholder: "jsmith"},
+                username: {label: "Username", type: "text", placeholder: "username"},
                 password: {label: "Password", type: "password"}
             },
             async authorize(credentials, req) {
@@ -22,4 +22,6 @@ export default NextAuth({
         })
     ],
     adapter: SequelizeAdapter(sequelize),
-})
+}
+
+export default NextAuth(options)

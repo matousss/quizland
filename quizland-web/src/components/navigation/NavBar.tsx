3bfc14372c735e3142ed15ce273a27ab5aa0f73b
matousss <matousss33@gmail.com>
2023-02-16 18:11:47 +0100

    Fixed nav align

--- quizland-web/src/components/navigation/NavBar.tsx
+++ quizland-web/src/components/navigation/NavBar.tsx
@@ -163,7 +163,7 @@ const NavBarBody = ({
         boolean
 }) => {
     return (
-        <div className={"mx-auto max-w-7xl px-2 sm:px-6 lg:px-8"}>
+        <div className={"m max-w-7xl px-2 sm:px-6 lg:px-8"}>
             <div className={"relative flex h-16 items-center justify-between"}>
                 <div className={"absolute inset-y-0 left-0 flex items-center sm:hidden"}>
                     <ToggleButton open={open}/>

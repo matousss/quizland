2ccc0643d21b7dae56ebef46e45799fb6e9b36a4
matousss <matousss33@gmail.com>
2023-02-25 00:39:12 +0100

    Flip cards

--- quizland-web/src/pages/cardset/index.tsx
+++ quizland-web/src/pages/cardset/index.tsx
@@ -1,33 +1,79 @@
 import {Section, SectionContainer} from "../../components/sections";
-import {createRef, FC, Key, MutableRefObject, useEffect, useRef, useState} from "react";
+import {
+    FC,
+    Key,
+    useEffect,
+    useRef,
+    useState,
+    PropsWithChildren
+} from "react";
 import {ChevronLeftIcon, ChevronRightIcon} from "@heroicons/react/20/solid";
+import {Transition} from "@headlessui/react";
+import styles from 'src/styles/Card.module.css'
+
+type TCard = [Array<string>, Array<string>]
 
 
-type TCard = [string, Array<string>]
-const Card: FC<{ card: TCard }> = ({card}) => {
+const AnimationCard: FC<{ key: Key, success: boolean, onAnimationEnd: () => void }> = ({success, ...props}) => (
+    <div
+        className={'opacity-0 font-sans uppercase bg-gray-600 absolute z-10 w-full h-full text-[4rem] rounded-lg text-gray-500 flex drop-shadow-lg border-[3.5px] '
+            + (success ? 'border-green-800 text-green-700 animate-card_r ' + styles.strokeGreen : 'border-red-800 text-red-700 animate-card_l ' + styles.strokeRed)}
+        {...props}
+    >
+        <div className={'m-auto'}>
+            {success ? '✓' : '✗'}
+        </div>
+    </div>
+)
+
+const CardFace: FC<{ flipped: boolean } & PropsWithChildren> = ({children, flipped}) => (
+    <Transition
+        show={flipped}
+        enterFrom={styles.flip}
+
+        leaveTo={styles.flip}
+
+        className={'flex py-6 bg-gray-600 absolute z-10 w-full h-full rounded-lg transition-transform duration-500 backface-hidden font-sans text-lg md: text-[2rem] text-gray-200'}
+    >
+        <div className={'px-8 m-auto divide-y divide-gray-500 overflow-y-auto max-h-full w-full scrollbar scrollbar-thumb-gray-500 scrollbar-thin scrollbar-w-[5px] scrollbar-rounded-[100%]'}>
+            {children}
+        </div>
+
+    </Transition>
+)
+
+const arrayToTerms = (array: Array<String>) => array.map((d, i) => (<div key={i} className={'text-center p-6'}>{d}</div>))
+const FlipCard: FC<{ card: TCard }> = ({card}) => {
     const [flipped, setFipped] = useState(false)
-    let definition = card[1].map((d, i) => (<div key={i}>{d}</div>))
-    let term = <div>{card[0]}</div>
+    let definition = arrayToTerms(card[1])
+    let term = arrayToTerms(card[0])
+    let cardRef = useRef<HTMLDivElement>(null)
+    const [animate, setAnimate] = useState(true)
+
+    useEffect(() => {
+        if (cardRef.current === null) return
+        setAnimate(true)
+
+    }, [card])
 
     return (
-        <div className={'bg-gray-600 relative rounded-lg h-[18rem] cursor-pointer'} onClick={() => setFipped(!flipped)}>
-            <div className={'font-sans uppercase animate-card bg-gray-600 absolute z-10 w-full h-full text-[4.5rem] rounded-lg text-gray-500 stroke-2 stroke-green-700 flex drop-shadow-lg'}
-
+        <div className={'w-[15rem] sm:w-[21rem] md:w-[27rem] select-none'}>
+            <div ref={cardRef} key={card[0][0]} className={'animate-from_left relative rounded-lg h-[10rem] sm:h-[14rem] md:h-[18rem] cursor-pointer backdrop-shadow-lg'}
             >
-                <style>
-                    {/*todo replace*/}
-                    {
-                        '#a {\n' +
-                        '                    -webkit-text-stroke-width: 1px;\n' +
-                        '                    -webkit-text-stroke-color: green;\n' +
-                        '                }'}
-                </style>
-                <div className={'m-auto'} id={'a'}>
-                    Known!
-                </div>
-            </div>
-            {flipped ? definition : term}
+                <div onClick={() => setFipped(!flipped)}>
 
+                    <CardFace flipped={flipped}>
+                        {term}
+                    </CardFace>
+                    <CardFace flipped={!flipped}>
+                        {definition}
+                    </CardFace></div>
+
+                {false && animate &&
+                    <AnimationCard onAnimationEnd={() => setAnimate(false)} success={!flipped} key={'animation'}/>}
+
+
+            </div>
         </div>
     )
 }
@@ -45,8 +91,9 @@ export default function Temp() {
     const title = "Slova 1";
     const description = "Tohle je  sada na učení kokotů"
     const cards: Array<TCard> = [
-        ["ahoj", ["pozdrav", "námořnický pozdrav"]],
-        ["píča", ["ženské pohlavní ústrojí", "nadávka"]]
+        [["ahoj"], ["pozdrav", "námořnický pozdrav", "pozdrav hovad", "nevim", "černá hra"]],
+        [["píča"], ["ženské pohlavní ústrojí", "nadávka"]],
+        [["zapnout svělo"], ["to turn the light on", "to flip the light on", "to flick the light on", "to switch on the light", "to snap the light on"]],
     ]
 
     const [index, setIndex] = useState(0)
@@ -62,11 +109,11 @@ export default function Temp() {
     return (
         <SectionContainer>
             <Section>
-                <div className={'m-5 mt-12 flex grow'}>
+                <div className={'sm:m-5 mt-12 flex grow'}>
                     <CardBtn onClick={() => setIndex(index - 1)}/>
-                    <div className={'w-full sm:w-2/3 md:w-1/3'}>
-                        <Card card={currentCard}/>
-                    </div>
+
+                    <FlipCard card={currentCard}/>
+
                     <CardBtn onClick={() => setIndex(index + 1)} flipped={true}/>
                 </div>
 
--- quizland-web/tailwind.config.js
+++ quizland-web/tailwind.config.js
@@ -1,55 +1,74 @@
 const cardAnimation = (rotation, x, y) => ({
-  '0%': {opacity: '100%'},
-  '30%': {
-    opacity: '80%'
-  },
-  '10%': {
-    transform: `rotate(${rotation}deg) size(1.05)`,
-  },
-  '50%':{transform: `translate(0,0) rotate(${rotation}deg) `},
-  '80%':{transform: `translate(${x/2}%,${y/2}%) rotate(${rotation}deg) `},
-  '100%': {
-    opacity: '0%',
-    transform: `translate(${x}%, ${y}%) rotate(8deg)`
-  }
+    '0%': {},
+    '5%': {
+        transform: `rotate(${rotation}deg) size(1.05)`,
+    },
+    '30%': {
+        opacity: '100%'
+    },
+    '50%': {transform: `translate(0,0) rotate(${rotation}deg)`},
+    '65%': {transform: `translate(${x / 3}%,${y / 3}%) rotate(${rotation}deg)`},
+    '80%': {transform: `translate(${x / 3 * 2}%,${y / 3 * 2}%) rotate(${rotation}deg)`},
+    '100%': {
+        opacity: '0%',
+        transform: `translate(${x}%, ${y}%) rotate(8deg)`
+    }
 })
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        primary: "rgb(9, 107, 82)",
-        secondary: "rgb(0,0,0)",
-        tercial: "rgb(0, 184, 107)",
-        text_a: "rgb(255,255,255)",
-      },
-      keyframes: {
-        card_old: {
-          '0%': {opacity: '100%'},
-          '30%': {
-            opacity: '80%'
-          },
-          '40%': {
-            transform: 'rotate(8deg) size(1.1)',
-          },
-          '100%': {
-            opacity: '0%',
-            transform: 'translate(12%, -15%) rotate(8deg)',
-          },
-          card_r: cardAnimation(8, 20, 0),
-          card_l: cardAnimation(-8, -20, 0)
+    content: [
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: "rgb(9, 107, 82)",
+                secondary: "rgb(0,0,0)",
+                tercial: "rgb(0, 184, 107)",
+                text_a: "rgb(255,255,255)",
+            },
+            keyframes: {
+                card_old: {
+                    '0%': {opacity: '100%'},
+                    '40%': {
+                        transform: 'rotate(8deg) size(1.1)',
+                        opacity: '100%',
+                    },
+                    '100%': {
+                        opacity: '0%',
+                        transform: 'translate(12%, -15%) rotate(8deg)',
+                    },
+                },
+                card_r: cardAnimation(-4, 20, 0),
+                card_l: cardAnimation(4, -20, 0),
+                from_left: {
+                    '0%': {
+                        transform: 'translate(-20%, 0)',
+                    },
+                    '100%': {
+                        transform: 'translate(0, 0)',
+                    }
+                },
+                from_right: {
+                    '0%': {
+                        transform: 'translate(20%, 0)',
+                    },
+                    '100%': {
+                        transform: 'translate(0, 0)',
+                    }
+                },
+            },
+            animation: {
+                card_r: 'card_r 800ms ease-in-out',
+                card_l: 'card_l 800ms ease-in-out',
+                from_left: 'from_left 300ms ease-in-out',
+                from_right: 'from_right 300ms ease-in-out',
+            },
         },
-      },
-      animation: {
-        card: 'cardd 1000ms ease-in-out infinite'
-      },
     },
-  },
-  plugins: [
-    require('@headlessui/tailwindcss')
-  ],
+    plugins: [
+        require('@headlessui/tailwindcss'),
+        require('tailwind-scrollbar')({ nocompatible: true }),
+    ],
 };

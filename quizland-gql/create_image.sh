243cc70315433dfdaa869a2a880ea0944ffe9963
matousss <matousss33@gmail.com>
2023-05-03 23:44:20 +0200

    Small fixes in scripts

--- quizland-gql/create_image.sh
+++ quizland-gql/create_image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-yarn build
-DOCKER_BUILDKIT=1 docker build . --tag quizland-api:0.1.1
+#yarn build
+DOCKER_BUILDKIT=1 docker build . --tag quizland-api:0.1.3
---
+++ quizland-web/create_image.sh
@@ -0,0 +1,1 @@
+docker build --tag quizland-www:0.1.3 --network host  .
